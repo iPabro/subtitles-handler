0cc19b25d22304edc1f241ef717c540b2bbd8d11
iPabro <pavelbrov@gmail.com>
2024-11-25 04:44:48 +0400

    updated lock file, removed phpunit and in previous commit was restructuring

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ipabro/subtitles-converter',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '86ad38619ca82eeca4f5336f27d953850e34f01b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ipabro/subtitles-converter' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '86ad38619ca82eeca4f5336f27d953850e34f01b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
